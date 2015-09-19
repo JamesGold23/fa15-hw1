4abadabfb8c076dd09bc940bda57ed446fec2914
James Goff <jsgoff23@gmail.com>
2015-09-18 20:54:07 -0700

    hw1 submission

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,7 @@
 class Foobar
-  # Q4 CODE HERE
+  def Foobar.baz(arr)
+  	sum = 0
+  	arr.map { |e| e.to_i + 2 }.keep_if { |e| e % 2 == 0 }.uniq.delete_if { |e| e > 10 }.each { |e| sum += e }
+  	sum
+  end
 end
